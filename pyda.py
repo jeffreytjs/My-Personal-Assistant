a0ec2a7b3632bc7e9c7c299e1923d14e6bc825e3
Jeffrey <jeffreytan_1996@hotmail.com>
2020-01-20 20:20:09 +0800

    Wolframalpha API

---
+++ pyda.py
@@ -0,0 +1,10 @@
+import wolframalpha
+
+input = raw_input("QUESTION: ")
+app_id = "YK36GA-W36R83QG5E"
+client = wolframalpha.Client(app_id)
+
+res = client.query(input)
+answer = next(res.results).text
+
+print(answer)
